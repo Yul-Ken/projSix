788cbc84cbdde3d516e8073d840cd7edb4f007a2
Yul-Ken <2426670028@qq.com>
2018-08-29 11:40:33 +0800

    Update catmull_Rom_Spline.cs

--- catmull_Rom_Spline.cs
+++ catmull_Rom_Spline.cs
@@ -30,12 +30,12 @@ public class catmull_Rom_Spline : MonoBehaviour {
 
     Vector3 catmull_Rom_Spline (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) //t=0时返回p1,t=1时返回p2
     {
-        Vector3 a = 2f * p1;
-		Vector3 b = p2 - p0;
-		Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
-		Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
-		Vector3 ret = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
+        Vector3 a = 2 * p1;
+	Vector3 b = p2 - p0;
+	Vector3 c = 2 * p0 - 5 * p1 + 4 * p2 - p3;
+	Vector3 d = -p0 + 3 * p1 - 3 * p2 + p3;
+	Vector3 ret = (a + (b * t) + (c * t * t) + (d * t * t * t)) / 2;
 
-		return ret;
+	return ret;
     }
 }
